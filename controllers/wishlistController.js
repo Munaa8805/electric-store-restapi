af7a57eb1d3adab6b355c645abbdcb610168f89f
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2026-03-24 20:37:45 -0600

    toggle wishlist

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -1,7 +1,7 @@
 import { Wishlist } from '../models/Wishlist.js';
 import { Product } from '../models/Product.js';
 
-export const addToWishlist = async (req, res) => {
+export const toggleToWishlist = async (req, res) => {
     try {
         const { productId } = req.body;
         let wishlist = await Wishlist.findOne({ user: req.user.id });
@@ -9,11 +9,13 @@ export const addToWishlist = async (req, res) => {
             wishlist = await Wishlist.create({ user: req.user.id, products: [productId] });
         } else {
             if (wishlist.products.includes(productId)) {
-                return res.status(400).json({ success: false, message: 'Product already in wishlist' });
+                wishlist.products = wishlist.products.filter(product => product.toString() !== productId);
+            } else {
+                wishlist.products.push(productId);
             }
-            wishlist.products.push(productId);
             await wishlist.save();
         }
+        await wishlist.populate('products');
         res.status(200).json({ success: true, data: wishlist });
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
@@ -31,26 +33,4 @@ export const getWishlist = async (req, res) => {
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
     };
-};
-
-
-export const removeFromWishlist = async (req, res) => {
-    const { productId } = req.params;
-
-    try {
-        const wishlist = await Wishlist.findOneAndUpdate(
-            { userId: req.user.id },
-            { $pull: { products: productId } }, // Atomically removes the ID from the array
-            { new: true } // Returns the updated document
-        );
-
-        if (!wishlist) {
-            return res.status(404).json({ message: "Wishlist not found" });
-        }
-
-        res.status(200).json({ message: "Product removed", wishlist });
-    } catch (error) {
-        res.status(500).json({ message: "Error removing from wishlist", error });
-    }
-
 };
--- routes/wishlistRoutes.js
+++ routes/wishlistRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { addToWishlist, getWishlist, removeFromWishlist } from '../controllers/wishlistController.js';
+import { toggleToWishlist, getWishlist } from '../controllers/wishlistController.js';
 import { authenticate } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.post('/add', authenticate, addToWishlist);
+router.post('/toggle', authenticate, toggleToWishlist);
 router.get('/', authenticate, getWishlist);
-router.delete('/remove/:productId', authenticate, removeFromWishlist);
+
 
 export default router;
